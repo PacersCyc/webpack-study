d7f6efc5d3a00d18e30cee026f314191b15d8d9a
PacersCyc <452720855@qq.com>
2020-03-07 13:16:56 +0800

    实现一个bundler

---
+++ bundler/bundler.js
@@ -0,0 +1,81 @@
+const fs = require('fs')
+const path = require('path')
+const parser = require('@babel/parser')
+const traverse = require('@babel/traverse').default
+const babel = require('@babel/core')
+
+const moduleAnalyzer = (filename) => {
+  const content = fs.readFileSync(filename, 'utf-8');
+  const ast = parser.parse(content, {
+    sourceType: 'module',
+  })
+  const dependencies = {}
+  traverse(ast, {
+    ImportDeclaration({ node }) {
+      // console.log(node)
+      const dirname = path.dirname(filename)
+      const newFile = path.join(dirname, node.source.value)
+      console.log(newFile)
+      dependencies[node.source.value] = newFile
+    }
+  })
+  console.log(dependencies)
+  const { code } = babel.transformFromAst(ast, null, {
+    presets: [
+      '@babel/preset-env'
+    ]
+  })
+  // console.log(code)
+  return {
+    filename,
+    dependencies,
+    code
+  }
+}
+
+const makeDependenciesGraph = (entry) => {
+  const entryModule = moduleAnalyzer(entry)
+  const graphArray = [entryModule]
+  for(let i=0; i<graphArray.length; i++) {
+    const item = graphArray[i]
+    const { dependencies } = item
+    if (dependencies) {
+      for(let j in dependencies) {
+        graphArray.push(moduleAnalyzer(dependencies[j]))
+      }
+    }
+  }
+  const graph = {}
+  graphArray.forEach(item => {
+    graph[item.filename] = {
+      dependencies: item.dependencies,
+      code: item.code
+    }
+  })
+  return graph
+}
+
+const generateCode = (entry) => {
+  const graph = JSON.stringify(makeDependenciesGraph(entry))
+  return `
+    (function(graph){
+      function require(module) {
+        function localRequire(relativePath) {
+          return require(graph[module].dependencies[relativePath])
+        }
+
+        var exports = {};
+        (function(require, exports, code){
+          eval(code)
+        })(localRequire, exports, graph[module].code);
+        return exports;
+      }
+      require('${entry}')
+    })(${graph})
+  `
+}
+
+// const graphInfo = makeDependenciesGraph('./src/index.js')
+// console.log(graphInfo)
+const code = generateCode('./src/index.js')
+console.log(code)
